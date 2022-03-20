8c6426ea9149e7481b462075e01e47bdb481d4df
parkhohyun <haor3@hanmail.net>
2022-03-21 00:00:54 +0900

    포켓몬 구성

---
+++ week2/PoketmonTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  PoketmonTableViewCell.swift
+//  week2
+//
+//  Created by 박호현 on 2022/03/20.
+//
+
+import UIKit
+
+class PoketmonTableViewCell: UITableViewCell {
+    @IBOutlet weak var pocketMonviewimage: UIImageView!
+    @IBOutlet weak var pocketMonviewname: UILabel!
+    @IBOutlet weak var pocketMonviewtype: UILabel!
+    @IBOutlet weak var pocketMonviewnumber: UILabel!
+}
--- week2/ViewController.swift
+++ week2/ViewController.swift
@@ -7,13 +7,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 155
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.row == 0 {
+            var cell = tableView.dequeueReusableCell(withIdentifier: "pocketmoncellView", for: indexPath) as! PoketmonTableViewCell
+            cell.pocketMonviewname.text = "피카츄"
+            cell.pocketMonviewtype.text = "전기타입"
+            cell.pocketMonviewnumber.text = "#025"
+            return cell
+        }
+        else if indexPath.row == 1 {
+            var cell = tableView.dequeueReusableCell(withIdentifier: "pocketmoncellView", for: indexPath) as! PoketmonTableViewCell
+            cell.pocketMonviewname.text = "거북이"
+            cell.pocketMonviewtype.text = "물타입"
+            cell.pocketMonviewnumber.text = "#033"
+            return cell
+        }
+        else {
+            var cell = tableView.dequeueReusableCell(withIdentifier: "pocketmoncellView", for: indexPath) as! PoketmonTableViewCell
+            cell.pocketMonviewname.text = "아몰랑"
+            cell.pocketMonviewtype.text = "몰랑타입"
+            cell.pocketMonviewnumber.text = "#0??"
+            return cell
+        }
+        
+    }
+    
 }
 
